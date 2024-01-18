0da153049b5477d36c7c38bc37661639a996e7fb
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2024-01-18 21:10:33 +0530

    import java.util.Scanner; public class mo{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int t=sc.nextInt();         while(t-->0){           int r1=sc.nextInt();           int r2=sc.nextInt();           int r3=sc.nextInt();           boolean b=false;           if((r1+r2)<r3||(r1+r3)<r2||(r2+r3)<r1)           b=true;           if(b)           System.out.println("YES");           else           System.out.println("NO");         }     } }

---
+++ Monopoly.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class mo{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+          int r1=sc.nextInt();
+          int r2=sc.nextInt();
+          int r3=sc.nextInt();
+          boolean b=false;
+          if((r1+r2)<r3||(r1+r3)<r2||(r2+r3)<r1)
+          b=true;
+          if(b)
+          System.out.println("YES");
+          else
+          System.out.println("NO");
+        }
+    }
+}
