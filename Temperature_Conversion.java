69be7e4b3088f15cd0e62b064da47fd695ff6d4a
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2024-02-05 10:43:59 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int c=sc.nextInt();         double F=32+(c*1.8);         System.out.printf("%.2f",F);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int c=sc.nextInt();
+        double F=32+(c*1.8);
+        System.out.printf("%.2f",F);
+    }
+}
