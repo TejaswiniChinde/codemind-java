4948e48752af031e0ba8353b35634c901419326e
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2024-02-05 12:12:46 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         float x=sc.nextFloat();         float y=sc.nextFloat();         double z=x*y;         System.out.printf("%.2f",z);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        float x=sc.nextFloat();
+        float y=sc.nextFloat();
+        double z=x*y;
+        System.out.printf("%.2f",z);
+    }
+}
