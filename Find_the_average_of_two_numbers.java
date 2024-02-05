87010137d41ab0519044458b6be524dc6cb70047
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2024-02-05 12:07:34 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double a=sc.nextInt();         double b=sc.nextInt();         double c=(a+b)/2;         System.out.printf("%.4f",c);
        }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextInt();
+        double b=sc.nextInt();
+        double c=(a+b)/2;
+        System.out.printf("%.4f",c);
+        
+    }
+}
