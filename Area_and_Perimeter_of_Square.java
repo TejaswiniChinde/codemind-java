6db1771b8a52379fc31938f524061af4d2f8b75e
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2024-02-06 10:48:45 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int s=sc.nextInt();         int are=s*s;         int per=4*s;         System.out.printf("%d %d",are,per);     } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        int are=s*s;
+        int per=4*s;
+        System.out.printf("%d %d",are,per);
+    }
+}
