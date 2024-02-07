34b3c3aa2533bcc71d809cb897f14a134415d45f
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2024-02-07 11:57:11 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int N=sc.nextInt();         int M=sc.nextInt();         int U=N*5+M*7;         System.out.print(U);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int M=sc.nextInt();
+        int U=N*5+M*7;
+        System.out.print(U);
+    }
+}
