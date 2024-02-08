faedcd579d93a99f5703f2ee8452c4b7c99a32c2
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2024-02-08 21:44:19 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int max=x*y;         System.out.println(max);     } }

---
+++ Instant_Noodles.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int max=x*y;
+        System.out.println(max);
+    }
+} 
