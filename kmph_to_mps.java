0a1354c3dbcb9cd512b2fd9a1b18acd5a56147f1
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2024-02-09 11:31:05 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc =new Scanner(System.in);         int km=sc.nextInt();         double m = 0.27778*km;         System.out.printf("%.2f",m);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int km=sc.nextInt();
+        double m = 0.27778*km;
+        System.out.printf("%.2f",m);
+    }
+}
