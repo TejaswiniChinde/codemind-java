68dec669d49cf9f24573daafabd433ec991a5e07
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2024-02-09 22:06:19 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         double c=a*2.54;         System.out.printf("%.2f",c);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        double c=a*2.54;
+        System.out.printf("%.2f",c);
+    }
+}
