e3ecbbd8d5bb83ce51156564900085f212b7c334
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2024-03-01 10:09:03 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int X=sc.nextInt();         int Y=sc.nextInt();         if(X>Y){             System.out.println(X);         }         else{             System.out.println(Y);         }     } }

---
+++ Height.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        int Y=sc.nextInt();
+        if(X>Y){
+            System.out.println(X);        
+        }
+        else{
+            System.out.println(Y);
+        }
+    }
+}
