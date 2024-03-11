8b74d96126325e1f16116cd63ca78f76cbe5e916
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2024-03-11 10:00:13 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int X=sc.nextInt();         int Y=sc.nextInt();         if(Y>X){             System.out.println(Y-X);
            }
            else{
                System.out.println(X-Y);
            }
            
          
                
            
        }
    }

---
+++ Donation.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        int Y=sc.nextInt();
+        if(Y>X){
+            System.out.println(Y-X);
+            
+        }
+        else{
+            System.out.println(X-Y);
+        }
+        
+      
+            
+        
+    }
+}
