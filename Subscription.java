493f472e65fdc2e4e914c096f69ee7649c8d8aeb
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2024-03-11 09:46:09 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int X=sc.nextInt();         if(X<=40){             System.out.println("NO");         }         else{             System.out.println("YES");         }     } }

---
+++ Subscription.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        if(X<=40){
+            System.out.println("NO");
+        }
+        else{
+            System.out.println("YES");
+        }
+    }
+}
